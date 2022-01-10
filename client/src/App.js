ac088f16c5c562c9c5ff375441e9ba84b93236c1
your name here <mmakhambetov@yandex.ru>
2022-01-11 01:54:40 +0400

    Set up WebSockets

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Home from './components/Home';
 import Game from './components/Game';
 
 const App = () => {
   const [playerName, setPlayerName] = useState('');
+
+  const webSocket = useRef(null);
+
+  useEffect(() => {
+    if (!playerName) return;
+
+    const ws = new WebSocket(process.env.REACT_APP_WS_URL);
+
+    ws.onopen = () => {
+      const message = {
+        type: 'add-new-player',
+        data: {
+          playerName,
+        },
+      };
+
+      ws.send(JSON.stringify(message));
+    };
+
+    ws.onmessage = (e) => {
+      console.log(e);
+    };
+
+    ws.onerror = (e) => {
+      console.error(e);
+    };
+
+    ws.onclose = (e) => {
+      console.log(e);
+    };
+
+    webSocket.current = { ws };
+
+    return function cleanup () {
+      if (webSocket.current !== null) {
+        webSocket.current.ws.close();
+      }
+    }
+  }, [playerName])
 
   const updatePlayerName = (playerName) => {
     setPlayerName(playerName);
--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const { createServer } = require('http');
 const morgan = require('morgan');
+const WebSocket = require('ws');
 
 const { port } = require('./config');
 
@@ -16,5 +17,33 @@ app.get('*', (req, res) => {
 });
 
 const server = createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const processIncomingMessage = (jsonData, ws) => {
+  console.log(`Processing incoming message ${jsonData}...`);
+  const message = JSON.parse(jsonData);
+
+  switch (message.type) {
+    case 'add-new-player':
+      addNewPlayer(message.data.player, ws);
+      break;
+    default:
+      throw new Error(`Unknown message type: ${message.type}`);
+  }
+};
+
+const addNewPlayer = (playerName, ws) => {
+  // TODO Handle adding the new player.
+};
+
+wss.on('connection', (ws) => {
+  ws.on('message', (jsonData) => {
+    processIncomingMessage(jsonData, ws);
+  });
+
+  ws.on('close', () => {
+    // TODO Cleanup the player that's associated with this WS.
+  });
+});
 
 server.listen(port, () => console.log(`Listening on http://localhost:${port}`));
